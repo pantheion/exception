fcb1fc9477bb450d317f7b662bcb2e9832d3b24f
Pedro Miguel <pedro.aos19@gmail.com>
2020-06-05 19:34:58 +0100

    init

---
+++ Exception.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Pantheion\Charon\Custom;
+
+class Exception extends \Exception
+{
+    public function __construct($message, $code = 0, $previous = null)
+    {
+        // $this->trace = $this->getTrace();
+        parent::__construct($message, $code, $previous);
+
+        $this->setClassInfo();
+        $this->setRequestInfo();
+        // die(var_dump($this));
+
+    }
+
+    protected function setClassInfo()
+    {
+        $class = new \ReflectionClass(Exception::class);
+
+        $this->name = $class->getName();
+        $this->shortName = $class->getShortName();
+        $this->namespace = $class->getNamespaceName();
+    }
+
+    protected function setRequestInfo()
+    {
+        $this->protocol = !empty($_SERVER['HTTPS']) ? 'https' : 'http';
+        $this->host = $_SERVER['HTTP_HOST'];
+        $this->url = $_SERVER['REQUEST_URI'];
+
+        $this->directory = $_SERVER['DOCUMENT_ROOT'];
+    }
+
+    public function resolveUrl()
+    {
+        return $this->protocol . "://" . $this->host . $this->url;
+    }
+}
